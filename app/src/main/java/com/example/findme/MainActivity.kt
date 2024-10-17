9a875b618500660d53b7ce998e60b096b1d4b2dd
Eric Miranda <eric.miranda.ext@siemens-energy.com>
2024-10-17 16:07:36 +0530

    location requests code

--- app/src/main/java/com/example/findme/MainActivity.kt
+++ app/src/main/java/com/example/findme/MainActivity.kt
@@ -2,13 +2,13 @@ package com.example.findme
 
 import android.Manifest
 import android.annotation.SuppressLint
+import android.content.Intent
 import android.os.Bundle
 import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
@@ -19,19 +19,22 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.findme.ui.theme.FindMeTheme
 import com.google.accompanist.permissions.ExperimentalPermissionsApi
 import com.google.accompanist.permissions.rememberMultiplePermissionsState
-import com.google.accompanist.permissions.rememberPermissionState
-import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationCallback
+import com.google.android.gms.location.LocationRequest
+import com.google.android.gms.location.LocationResult
 import com.google.android.gms.location.LocationServices
+import com.google.android.gms.location.Priority
 import com.google.android.gms.maps.model.CameraPosition
 import com.google.android.gms.maps.model.LatLng
 import com.google.maps.android.compose.GoogleMap
+import com.google.maps.android.compose.Marker
 import com.google.maps.android.compose.rememberCameraPositionState
 import com.google.maps.android.compose.rememberMarkerState
+import java.util.concurrent.TimeUnit
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -51,14 +54,13 @@ class MainActivity : ComponentActivity() {
 @OptIn(ExperimentalPermissionsApi::class)
 @Composable
 fun Map(modifier: Modifier = Modifier) {
-    Column (modifier = modifier) {
+    Column(modifier = modifier) {
         val context = LocalContext.current
 
         val locationPermissionsState = rememberMultiplePermissionsState(
             // add the permissions you want here
             listOf(
-                Manifest.permission.ACCESS_COARSE_LOCATION,
-                Manifest.permission.ACCESS_FINE_LOCATION
+                Manifest.permission.ACCESS_COARSE_LOCATION, Manifest.permission.ACCESS_FINE_LOCATION
             )
         )
 
@@ -66,19 +68,41 @@ fun Map(modifier: Modifier = Modifier) {
             Column {
                 Text(text = "All location permissions granted!")
 
-                // we can check for locations now
                 val fusedLocationClient = LocationServices.getFusedLocationProviderClient(context)
-                val cameraPositionState = rememberCameraPositionState {}
+                val cameraPositionState = rememberCameraPositionState()
+                val locationMarkerState = rememberMarkerState()
+                Log.d("defaults", "${cameraPositionState.position}, $locationMarkerState")
 
-                fusedLocationClient.lastLocation.addOnSuccessListener {
-                    Log.d("pos", it.toString())
-                    cameraPositionState.position = CameraPosition.fromLatLngZoom(LatLng(it.latitude, it.longitude), 20f)
+                // this time, let's subscribe for location updates
+                val locationRequest = LocationRequest
+                    .Builder(TimeUnit.SECONDS.toMillis(10))
+                    .setPriority(Priority.PRIORITY_HIGH_ACCURACY).build()
+
+                val locationCallback = object : LocationCallback() {
+                    override fun onLocationResult(locationUpdate: LocationResult) {
+                        super.onLocationResult(locationUpdate)
+                        Log.d("location update", locationUpdate.lastLocation.toString())
+                        val updatedLatLng = LatLng(
+                            locationUpdate.lastLocation!!.latitude,
+                            locationUpdate.lastLocation!!.longitude
+                        )
+                        cameraPositionState.position =
+                            CameraPosition.fromLatLngZoom(updatedLatLng, 15f)
+                        locationMarkerState.position = updatedLatLng
+                    }
                 }
 
+                fusedLocationClient.requestLocationUpdates(locationRequest, locationCallback, null)
+
                 GoogleMap(
-                    modifier = Modifier.fillMaxSize(),
-                    cameraPositionState = cameraPositionState
-                )
+                    modifier = Modifier.fillMaxSize(), cameraPositionState = cameraPositionState
+                ) {
+                    Marker(
+                        state = locationMarkerState,
+                        title = "Current location",
+                        snippet = "Marker of current location"
+                    )
+                }
             }
         } else {
             val allPermissionsRevoked =
